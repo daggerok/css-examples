ec0ec04ac4189f8cbb447df0b6b13910573d7b98
Maksim Kostromin <daggerok@gmail.com>
2020-02-11 22:44:29 +0200

    Implement menu.

--- css-gradient-based-app-step-1/src/js/app.js
+++ css-gradient-based-app-step-1/src/js/app.js
@@ -9,3 +9,28 @@ WebFont.load({
     ],
   },
 });
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function findMenu() {
+  return $('aside nav ul');
+}
+
+function findImg() {
+  return $('img');
+}
+
+function toggleMenu(e) {
+  const menu = findMenu();
+  // menu.display = !menu || !menu.display || menu.display !== 'block' ? 'block' : 'none';
+  menu.classList.toggle('active');
+  e.preventDefault();
+}
+
+function onDomContentLoaded() {
+  findImg().addEventListener('click', toggleMenu, false);
+}
+
+document.addEventListener('DOMContentLoaded', onDomContentLoaded, false);
