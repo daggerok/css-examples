0e5327da0a3f0e866941624a4f6118df0c4c1a67
Maksim Kostromin <daggerok@gmail.com>
2020-03-27 04:33:03 +0200

    Refactor factories, revisit visitors (C) whatthecommit.com

--- css-theme/src/app.ts
+++ css-theme/src/app.ts
@@ -10,14 +10,18 @@ WebFont.load({
 
 const light: HTMLLinkElement = document.querySelector('#light');
 const dark: HTMLLinkElement = document.querySelector('#dark');
+
+const theme = localStorage.getItem('theme');
 const body: HTMLElement = document.body;
+body.classList.add(theme ? theme : 'dark');
 
 dark.onclick = e => {
   body.classList.remove('light');
   body.classList.add('dark');
+  localStorage.setItem('theme', 'dark');
 };
 
 light.onclick = e => {
-  body.classList.remove('dark');
-  body.classList.add('light');
+  body.classList.replace('dark', 'light');
+  localStorage.setItem('theme', 'light');
 };
