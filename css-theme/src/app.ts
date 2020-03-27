1b2d899894aad2a876f785877ae4b259d766ff26
Maksim Kostromin <daggerok@gmail.com>
2020-03-27 04:18:38 +0200

    SHIT ===> GOLD (C) whatthecommit.com

--- css-theme/src/app.ts
+++ css-theme/src/app.ts
@@ -7,3 +7,17 @@ WebFont.load({
     ],
   },
 });
+
+const light: HTMLLinkElement = document.querySelector('#light');
+const dark: HTMLLinkElement = document.querySelector('#dark');
+const body: HTMLElement = document.body;
+
+dark.onclick = e => {
+  body.classList.remove('light');
+  body.classList.add('dark');
+};
+
+light.onclick = e => {
+  body.classList.remove('dark');
+  body.classList.add('light');
+};
